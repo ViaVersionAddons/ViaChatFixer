ee4b60e89354c9405eef7488ef66e0a5644da0fa
MrMicky <git@mrmicky.fr>
2020-06-25 15:19:42 +0200

    Update to ViaVersion 3.0 for 1.16 support

--- common/src/main/java/fr/mrmicky/viachatfixer/handlers/via/ViaChatHandler.java
+++ common/src/main/java/fr/mrmicky/viachatfixer/handlers/via/ViaChatHandler.java
@@ -2,21 +2,18 @@ package fr.mrmicky.viachatfixer.handlers.via;
 
 import fr.mrmicky.viachatfixer.ViaChatFixerPlatform;
 import fr.mrmicky.viachatfixer.handlers.ChatHandler;
-import us.myles.ViaVersion.api.PacketWrapper;
 import us.myles.ViaVersion.api.Via;
 import us.myles.ViaVersion.api.data.UserConnection;
 import us.myles.ViaVersion.api.protocol.Protocol;
 import us.myles.ViaVersion.api.protocol.ProtocolRegistry;
 import us.myles.ViaVersion.api.protocol.ProtocolVersion;
-import us.myles.ViaVersion.api.remapper.PacketHandler;
 import us.myles.ViaVersion.api.remapper.PacketRemapper;
 import us.myles.ViaVersion.api.type.Type;
 import us.myles.ViaVersion.packets.State;
 import us.myles.ViaVersion.protocols.protocol1_11to1_10.Protocol1_11To1_10;
+import us.myles.ViaVersion.protocols.protocol1_9_3to1_9_1_2.ServerboundPackets1_9_3;
 
-import java.lang.reflect.Field;
 import java.util.HashSet;
-import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
 
@@ -24,66 +21,54 @@ public class ViaChatHandler implements ChatHandler {
 
     private final Set<UUID> unknownPlayers = new HashSet<>();
 
-    private ViaChatFixerPlatform platform;
+    private final ViaChatFixerPlatform platform;
 
     public ViaChatHandler(ViaChatFixerPlatform platform) {
         this.platform = platform;
     }
 
     @Override
-    public void init() throws Exception {
+    public void init() {
         if (ProtocolRegistry.SERVER_PROTOCOL >= ProtocolVersion.v1_11.getId()) {
             platform.getLogger().warning("This plugin is not required on 1.11+ servers, you can just remove it :)");
+            return;
         }
-
-        Field registryMapField = ProtocolRegistry.class.getDeclaredField("registryMap");
-        registryMapField.setAccessible(true);
 
         //noinspection unchecked
-        Map<Integer, Map<Integer, Protocol>> registryMap = (Map<Integer, Map<Integer, Protocol>>) registryMapField.get(null);
-
-        Protocol protocol = null;
-        for (Map<Integer, Protocol> protocolMap : registryMap.values()) {
-            for (Protocol prot : protocolMap.values()) {
-                if (prot instanceof Protocol1_11To1_10) {
-                    protocol = prot;
-                    break;
-                }
-            }
-        }
+        Protocol<?, ?, ?, ServerboundPackets1_9_3> protocol = ProtocolRegistry.getProtocol(Protocol1_11To1_10.class);
 
         if (protocol == null) {
-            throw new RuntimeException("Protocol 1_11To1_10 not found");
+            throw new IllegalStateException("Protocol 1_11To1_10 not found");
         }
 
         protocol.registerIncoming(State.PLAY, 0x02, 0x02, new PacketRemapper() {
             @Override
             public void registerMap() {
                 map(Type.STRING); // 0 - Message
-                handler(new PacketHandler() {
-                    @Override
-                    public void handle(PacketWrapper wrapper) throws Exception {
-                        String msg = wrapper.get(Type.STRING, 0);
+                handler(wrapper -> {
+                    // 100 character limit on older servers
+                    String message = wrapper.get(Type.STRING, 0);
 
-                        if (msg.length() > 100) {
-                            wrapper.set(Type.STRING, 0, msg.substring(0, 100));
-
-                            UserConnection connection = wrapper.user();
-                            ChatTracker chatTracker = connection.get(ChatTracker.class);
-
-                            if (chatTracker == null) {
-                                chatTracker = new ChatTracker(connection);
-                                connection.put(chatTracker);
-                            }
-
-                            // don't allow messages longer than 256 characters
-                            if (msg.length() > 256) {
-                                msg = msg.substring(0, 256);
-                            }
-
-                            chatTracker.updateLastMessage(msg);
-                        }
+                    if (message.length() <= 100) {
+                        return;
                     }
+
+                    wrapper.set(Type.STRING, 0, message.substring(0, 100));
+
+                    UserConnection connection = wrapper.user();
+                    ChatTracker chatTracker = connection.get(ChatTracker.class);
+
+                    if (chatTracker == null) {
+                        chatTracker = new ChatTracker(connection);
+                        connection.put(chatTracker);
+                    }
+
+                    // don't allow messages longer than 256 characters
+                    if (message.length() > 256) {
+                        message = message.substring(0, 256);
+                    }
+
+                    chatTracker.updateLastMessage(message);
                 });
             }
         }, true);
@@ -102,17 +87,19 @@ public class ViaChatHandler implements ChatHandler {
 
         ChatTracker chatTracker = connection.get(ChatTracker.class);
 
-        if (chatTracker != null && chatTracker.getLastMessage() != null) {
-            if (!chatTracker.isValid(100)) {
-                chatTracker.reset();
-                return null;
-            }
-
-            String message = chatTracker.getLastMessage();
-            chatTracker.reset();
-            return message;
+        if (chatTracker == null || chatTracker.getLastMessage() == null) {
+            return null;
         }
 
-        return null;
+        if (!chatTracker.isValid(100)) {
+            chatTracker.reset();
+            return null;
+        }
+
+        String message = chatTracker.getLastMessage();
+
+        chatTracker.reset();
+
+        return message;
     }
 }

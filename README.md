9ce9dd926db3734490684803cddbe26ae799f504
BT (calcastor/mame) <43831917+calcastor@users.noreply.github.com>
2024-06-19 08:17:47 -0700

    Update for ViaVersion v5.0 (#1)
    Co-authored-by: MrMicky <git@mrmicky.fr>

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 [![Java CI](https://github.com/MrMicky-FR/ViaChatFixer/actions/workflows/build.yml/badge.svg)](https://github.com/MrMicky-FR/ViaChatFixer/actions/workflows/build.yml)
 
-ViaChatFixer is a Bukkit and Sponge plugin that allows 1.11+ players to have longer chat messages on 1.8-1.10 servers with ViaVersion
+ViaChatFixer is a Bukkit plugin that allows 1.11+ players to have longer chat messages on 1.8-1.10 servers with ViaVersion
 
 In Minecraft 1.11, the maximum messages length has gone from 100 characters to 256 characters. So if your server is under 1.11, 1.11 and higher
 players will have their chat messages cut at 100 characters.
@@ -15,21 +15,22 @@ You can download releases and find more information on [SpigotMC](https://www.sp
 
 ## Installation
 
-Just put the ViaChatFixer jar in your plugins folder.
+Just put the ViaChatFixer jar in your `plugins` folder.
 
-ViaChatFixer currently supports ViaVersion v4.0 or higher.
+ViaChatFixer currently supports ViaVersion v5.0 or higher, and requires Java 17.
 
 All players online during a reload will not be able to have longer chat messages until they reconnect.
 
 ## Supported Platforms
 
-ViaChatFixer can be installed on Bukkit/Spigot/Paper servers or on Sponge servers.
+ViaChatFixer can be installed on Bukkit, Spigot, or Paper servers.
 
 If you are using a proxy, ViaVersion and ViaChatFixer must be installed on the backend servers.
 
 ### Versions compatibility
 
 | ViaChatFixer | ViaVersion | 
-| ------------ | ---------- |
-|    v1.0.0    |    v3.x    |
-|    v1.1.x    |    v4.x    |
+|--------------|------------|
+| v1.0.0       | v3.x       |
+| v1.1.0       | v4.x       |
+| v1.2.x       | v5.x       |
--- src/main/java/fr/mrmicky/viachatfixer/ChatHandler.java
+++ src/main/java/fr/mrmicky/viachatfixer/ChatHandler.java
@@ -1,13 +1,13 @@
-package fr.mrmicky.viachatfixer.common;
+package fr.mrmicky.viachatfixer;
 
 import com.viaversion.viaversion.api.Via;
 import com.viaversion.viaversion.api.connection.UserConnection;
 import com.viaversion.viaversion.api.protocol.Protocol;
 import com.viaversion.viaversion.api.protocol.packet.State;
-import com.viaversion.viaversion.api.protocol.remapper.PacketRemapper;
+import com.viaversion.viaversion.api.protocol.remapper.PacketHandlers;
 import com.viaversion.viaversion.api.protocol.version.ProtocolVersion;
-import com.viaversion.viaversion.api.type.Type;
-import com.viaversion.viaversion.protocols.protocol1_11to1_10.Protocol1_11To1_10;
+import com.viaversion.viaversion.api.type.Types;
+import com.viaversion.viaversion.protocols.v1_10to1_11.Protocol1_10To1_11;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -17,39 +17,41 @@ public class ChatHandler {
 
     private final Set<UUID> unknownPlayers = new HashSet<>();
 
-    private final ViaChatFixerPlatform platform;
+    private final ViaChatFixer plugin;
 
     private boolean enabled = false;
 
-    public ChatHandler(ViaChatFixerPlatform platform) {
-        this.platform = platform;
+    public ChatHandler(ViaChatFixer plugin) {
+        this.plugin = plugin;
     }
 
-    public void init() {
-        if (Via.getAPI().getServerVersion().lowestSupportedVersion() >= ProtocolVersion.v1_11.getVersion()) {
-            this.platform.getLoggerAdapter().warn("This plugin is not required on 1.11+ servers, you can just remove it.");
-            return;
+    public boolean init() {
+        ProtocolVersion lowestVersion = Via.getAPI().getServerVersion().lowestSupportedProtocolVersion();
+
+        if (lowestVersion.getVersion() >= ProtocolVersion.v1_11.getVersion()) {
+            this.plugin.getLogger().warning("This plugin is not required on 1.11+ servers, you can just remove it.");
+            return false;
         }
 
-        Protocol<?, ?, ?, ?> protocol = Via.getManager().getProtocolManager().getProtocol(Protocol1_11To1_10.class);
+        Protocol<?, ?, ?, ?> protocol = Via.getManager().getProtocolManager().getProtocol(Protocol1_10To1_11.class);
 
         if (protocol == null) {
-            throw new IllegalStateException("Protocol 1_11To1_10 not found");
+            throw new IllegalStateException("Protocol1_10To1_11 not found");
         }
 
-        protocol.registerServerbound(State.PLAY, 0x02, 0x02, new PacketRemapper() {
+        protocol.registerServerbound(State.PLAY, 0x02, 0x02, new PacketHandlers() {
             @Override
-            public void registerMap() {
-                map(Type.STRING); // 0 - Message
+            public void register() {
+                map(Types.STRING); // 0 - Message
                 handler(wrapper -> {
-                    // 100 character limit on older servers
-                    String message = wrapper.get(Type.STRING, 0);
+                    // 100 characters limit on older servers
+                    String message = wrapper.get(Types.STRING, 0);
 
                     if (message.length() <= 100) {
                         return;
                     }
 
-                    wrapper.set(Type.STRING, 0, message.substring(0, 100));
+                    wrapper.set(Types.STRING, 0, message.substring(0, 100));
 
                     UserConnection connection = wrapper.user();
                     ChatTracker chatTracker = connection.get(ChatTracker.class);
@@ -70,6 +72,8 @@ public class ChatHandler {
         }, true);
 
         this.enabled = true;
+
+        return true;
     }
 
     public String handle(UUID uuid) {
@@ -95,7 +99,7 @@ public class ChatHandler {
 
         if (connection == null) {
             if (this.unknownPlayers.add(uuid)) {
-                this.platform.getLoggerAdapter().warn("Unknown connection for player with UUID " + uuid);
+                this.plugin.getLogger().warning("Unknown connection for player with UUID " + uuid);
             }
 
             return null;
